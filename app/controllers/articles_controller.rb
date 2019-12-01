baebdf06f330ff9996add651e5f747d9a1446b36
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-01 13:14:25 -0500

    change flash msg, affects _messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@
         # render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
         @article = Article.new(article_params) #need to white list values of Article, with what's beeing passed in from params  hash
         if @article.save
-            flash[:notice] = "Article was successfully created" #show message to user
+            flash[:success] = "Article was successfully created" #show message to user
             redirect_to article_path(@article) # article GET /articles/:id article#show
         else
             render :new #render new view, to give user change to try again
@@ -27,7 +27,7 @@
     def update
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully updated"
+            flash[:success] = "Article was successfully updated"
             redirect_to article_path(@article) # redirect to Article show page, article GET /articles/:id article#show
         else # if did not pass validations
             render 'edit' #render EDIT template again, 2nd way to do this is with ('') instead of (:)
@@ -41,7 +41,7 @@
     def destroy
         # find article based on :id found in params hash 
         
-        flash[:notice] = "Article was successfully deleted"
+        flash[:danger] = "Article was successfully deleted"
         @article.destroy
         redirect_to articles_path
     end
