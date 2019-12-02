6bd191932572470a0472dca36c8a5089b765923d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-02 10:01:33 -0500

    Many ARTICLES belongs_to USER, in one-to-many

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
  class Article < ApplicationRecord
+
+   belongs_to :user # Many side of One-To=-Many, is singular as One USER(singular) has_many ARTICLES(plural)
+
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
  end
