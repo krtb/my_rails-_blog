89f7b8c2fc04202a3e9fa3be0db744b627398b3a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-02 09:21:07 -0500

    validate uniqueness of Users usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minumum: 3, maximum: 25 }
+    validates :username, presence: true, uniqueness: true, length: { minumum: 3, maximum: 25 }
 end
