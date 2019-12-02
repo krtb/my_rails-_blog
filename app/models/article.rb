5d70617c67b2630cecff3a8f90cc34bf2d036bb1
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-02 10:08:57 -0500

    remove tab

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@
 
    belongs_to :user # Many side of One-To=-Many, is singular as One USER(singular) has_many ARTICLES(plural)
 
-    validates :title, presence: true, length: {minimum: 3, maximum: 50}
-    validates :description, presence: true, length: {minimum: 10, maximum: 300}
-    validates :user_id, presence: true
+   validates :title, presence: true, length: {minimum: 3, maximum: 50}
+   validates :description, presence: true, length: {minimum: 10, maximum: 300}
+   validates :user_id, presence: true
  end
