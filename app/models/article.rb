de195e9b05ce5e82245d6188ab396c56d3bdfc38
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-02 10:03:15 -0500

    ensure user_id present, when Article created

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@
 
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
+    validates :user_id, presence: true
  end
