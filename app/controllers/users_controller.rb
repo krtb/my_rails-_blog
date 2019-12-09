925dc28ba1e096940bfa2a75f7690a5ec49f3bd5
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-09 14:09:18 -0500

    save new user to db, flash msg OR return new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+        # need to write user.save to save to database
+        if @user.save
+            # if user saves, want to display a message
+            flash[:success] = "Welcome to Kurt's rails blog, #{@user.username}"
+            # once saved, display list of articles that belong to that user
+            redirect_to articles_path
+        else
+            # if wrong info, return to new view template page to re-enter info
+            # will display error messages from errors partial
+            return 'new'
+        end
     end
 
     private
