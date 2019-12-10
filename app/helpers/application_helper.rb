f884d8fa83e4dd59f531360fe5ac22b9090012d1
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-10 13:09:51 -0500

    add default size of 80, custom size from view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-    def gravatar_for(user)
+    def gravatar_for(user, options = {size: 80})
         # gravatar urls are based on MD5 hash of user's email address
         # in Ruby, this is implemented using the HEX digest method
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         # img-circle from BootStrap 
         image_tag(gravatar_url, alt: user.username, class: "rounded-circle mx-auto d-block")
     end
