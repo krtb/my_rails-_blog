7b3f3c743c0b2e443e848247258aac3138e23e38
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-10 13:01:53 -0500

    add gravatar_for method helper, use in users/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def gravatar_for(user)
+        # gravatar urls are based on MD5 hash of user's email address
+        # in Ruby, this is implemented using the HEX digest method
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        # img-circle from BootStrap 
+        image_tag(gravatar_url, alt: user.username, class: "rounded-circle mx-auto d-block")
+    end
 end
