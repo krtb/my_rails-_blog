622fc8da1f410927b88b8bafe8a92c0510e73169
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-11 17:22:57 -0500

    change except to only

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@
     # before actions work in order
     before_action :set_article, only: [:edit, :update, :show, :destroy]
     before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def index
         @articles = Article.paginate(:page => params[:page], :per_page => 2)
@@ -68,5 +69,11 @@
         params.require(:article).permit(:title, :description)
     end
 
+    def require_same_user
+        if current_user != @article.user
+            flash[:danger] = "you can only edit or delte your own article "
+            redirect_to root_path
+        end
+    end
 
  end
