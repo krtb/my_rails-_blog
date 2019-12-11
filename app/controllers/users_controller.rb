9b1957d4f9c67d368d21f7affdc375393473cfce
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-11 15:25:19 -0500

    find user instance, before other actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show ]
 
     def index
         # display a list of all users, use paginate 
@@ -25,11 +26,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
 
         if @user.update(user_params)
             flash[:success] = "Your account was updated succesfully."
@@ -40,12 +39,14 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         # create an instance var of all users articles with pagination
         @user_articles = @user.articles.paginate(:page => params[:page], :per_page => 2)
     end
 
     private
+    def set_user
+        @user = User.find(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
