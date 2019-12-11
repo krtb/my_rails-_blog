8dfb9adf24b25812bfc04592d900da7b6ed73122
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-11 13:32:29 -0500

    add create and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   resources 'users', except: [:new]
 
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 end
