08f3e61a59b68b2f3c646bb9f08fa99149b9e205
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-11 17:04:28 -0500

    use helper method to check if user logged in

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
  class ArticlesController < ApplicationController
+    # before actions work in order
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
 
     def index
         @articles = Article.paginate(:page => params[:page], :per_page => 2)
