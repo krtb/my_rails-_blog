9d352b5b077f221bb7c17da6282a864ceab35385
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-11 16:08:28 -0500

    only show sign on home page if not loggedin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController # inherits/subclasses from Applica
 
     #actions ARE methods
     def home
+        redirect_to articles_path if logged_in?
     end
 
     def about
