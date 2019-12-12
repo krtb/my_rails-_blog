72d9272a3bcc7343ae15f974d066c79fe0ac6d4c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-12 16:59:07 -0500

    check of and admin, to delete user & articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -61,6 +62,12 @@ class UsersController < ApplicationController
     if current_user != @user && !current_user.admin?
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? && !current_user.admin? 
+      flash[:danger] = "Only admin users can perform that action."
     end
   end
   
