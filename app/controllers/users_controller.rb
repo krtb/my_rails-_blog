69ded052c4ad5d8d6378e560164132ba01d71e31
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-12 15:48:34 -0500

    set session to used id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,63 +1,67 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit, :update, :show ]
-    before_action :require_same_user, only: [:edit, :update ]
-
-    def index
-        # display a list of all users, use paginate 
-        @users = User.paginate(:page => params[:page], :per_page => 2)
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    # need to write user.save to save to database
+    if @user.save
+      session[:user_id] = @user.id
+      # if user saves, want to display a message
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
 
-    def new
-        @user = User.new
+  end
+  
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
+  
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
     end
-
-    def create
-        @user = User.new(user_params)
-        # need to write user.save to save to database
-        if @user.save
-            # if user saves, want to display a message
-            flash[:success] = "Welcome to Kurt's rails blog, #{@user.username}"
-            # once saved, display list of articles that belong to that user
-            redirect_to articles_path
-        else
-            # if wrong info, return to new view template page to re-enter info
-            # will display error messages from errors partial
-            return 'new'
-        end
-    end
-
-    def edit
-    end
-
-    def update
-
-        if @user.update(user_params)
-            flash[:success] = "Your account was updated succesfully."
-            redirect_to articles_path
-        else
-            return 'edit'
-        end 
-    end
-
-    def show
-        # create an instance var of all users articles with pagination
-        @user_articles = @user.articles.paginate(:page => params[:page], :per_page => 2)
-    end
-
-    private
-    def set_user
-        @user = User.find(params[:id])
-    end
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
-
-    def require_same_user
-        if  current_user !=  @user
-            flash[:danger] = "You can only edit your own account"
-            redirect_to root_path
-        end
-    end
-
+  end
+  
 end
